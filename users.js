ed8c604d27b0f9566441b5b93dda5a6f877e1226
Softcoderindia-code <78151067+Softcoderindia-code@users.noreply.github.com>
2021-05-26 14:38:45 +0530

    Update users.js

--- users.js
+++ users.js
@@ -112,6 +112,7 @@ function renderData(individualDoc) {
             if (user) {
                 fs.collection(user.uid).doc(id).delete();
             }
+            renderData()
         })
         
     })
