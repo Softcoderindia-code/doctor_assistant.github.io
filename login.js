53100302a12b176cf80251eb8a3e42d91b5f9086
Softcoderindia-code <78151067+Softcoderindia-code@users.noreply.github.com>
2021-07-06 16:16:24 +0530

    Update login.js

--- login.js
+++ login.js
@@ -12,3 +12,18 @@ loginForm.addEventListener('submit', e => {
         loginError.innerText = err.message;
     })
 })
+function forgot(){
+    firebase.auth().sendPasswordResetEmail(document.getElementById('login-email').value)
+            .then(() => {
+                const text = document.getElementById('loginError')
+                text.innerHTML = 'Email Sent'
+            })
+            .catch((error) => {
+              var errorCode = error.code;
+              var errorMessage = error.message;
+              console.log(errorMessage)
+              const text = document.getElementById('loginError')
+              text.innerHTML = errorMessage
+              // ..
+            });
+}
